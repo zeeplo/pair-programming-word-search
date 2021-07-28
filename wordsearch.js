b6ff7929c1c1d1e947306e2401f26604586f07eb
William Ye <w3ye@pm.me>
2021-07-28 17:49:06 -0400

    search horizontally, backwards, downwards OK

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (! horizontalSearch(letters, word)) {
+    if (! horizontalSearch(backwards(letters), word)) {
+      if (! horizontalSearch(transpose(letters), word)) return false;
+      return true;
+    } return true;
+  } return true;
+};
 
-module.exports = wordSearch
+const horizontalSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  for (let l of horizontalJoin) {
+    //console.log(l)
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+const backwards = (letters) => {
+  const backwardsJoin = letters.map(ls => ls.reverse());
+  return backwardsJoin;
+};
+
+const transpose = function(matrix) {
+  let array = [[]];
+  // loop from 0 -> row length
+  for (let i = 0; i < matrix[0].length; i++) {
+    array[i] = [];
+    // loop for 0 -> column length
+    for (let j = 0; j < matrix.length; j++) {
+      array[i][j] = matrix[j][i];
+    }
+  }
+  return array;
+};
